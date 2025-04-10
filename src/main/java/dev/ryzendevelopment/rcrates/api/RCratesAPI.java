f050244adff26b44e48b8960742c5826bbb85caa
Relahx <rhunterkyt@gmail.com>
2025-04-09 23:25:10 -0300

    successfully set all methods visible

--- src/main/java/dev/ryzendevelopment/rcrates/api/RCratesAPI.java
+++ src/main/java/dev/ryzendevelopment/rcrates/api/RCratesAPI.java
@@ -9,56 +9,58 @@ import java.util.List;
 import java.util.Map;
 
 public class RCratesAPI {
+
     public RCratesAPI(JavaPlugin plugin) {
         Plugin crates = plugin.getServer().getPluginManager().getPlugin("RCrates");
         if (crates == null) throw new IllegalArgumentException("RCrates is not loaded"); 
     }
 
-    List<String> getAllCrates() {
+    public List<String> getAllCrates() {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    boolean getCrate(String crateName) {
+    public boolean getCrate(String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    void openCrate(Player player, String crateName) {
+    public void openCrate(Player player, String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    List<ItemStack> getCrateRewards(String crateName) {
+    public List<ItemStack> getCrateRewards(String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    Map<Integer, Double> getCrateChancesRewards(String crateName) {
+    public Map<Integer, Double> getCrateChancesRewards(String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    Map<Integer, ItemStack> getCrateSlotRewards(String crateName) {
+    public Map<Integer, ItemStack> getCrateSlotRewards(String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    boolean addCrate(String crateName, Map<Integer, ItemStack> rewards) {
+    public boolean addCrate(String crateName, Map<Integer, ItemStack> rewards) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    boolean removeCrate(String crateName) {
+    public boolean removeCrate(String crateName) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    void giveKey(Player player, String crateName, int quantity) {
+    public void giveKey(Player player, String crateName, int quantity) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    void giveCrate(Player player, String crateName, int quantity) {
+    public void giveCrate(Player player, String crateName, int quantity) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    boolean hasKey(Player player, ItemStack item) {
+    public boolean hasKey(Player player, ItemStack item) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
 
-    boolean hasCrate(Player player, ItemStack item) {
+    public boolean hasCrate(Player player, ItemStack item) {
         throw new IllegalArgumentException("RCrates is not loaded");
     }
+
 }
